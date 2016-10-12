8eb40d234d823d89bc8b0a7c971daf0d5336478a
Xu Jiaqi <xujiaqi0830@163.com>
2016-10-12 19:04:13 +0800

    上传托管
    上传托管

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# jQuery-Teeterboard
-基于jQuery的扩展，为网页元素添加鼠标移动时，元素被按压下去的跷跷板效果
+[![GitHub license](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://raw.githubusercontent.com/xujiaqi0830/jQuery-Teeterboard/master/LICENSE)
+[![GitHub forks](https://img.shields.io/github/forks/xujiaqi0830/jQuery-Teeterboard.svg)](https://github.com/xujiaqi0830/jQuery-Teeterboard/network)
+[![GitHub stars](https://img.shields.io/github/stars/xujiaqi0830/jQuery-Teeterboard.svg)](https://github.com/xujiaqi0830/jQuery-Teeterboard/stargazers)
+
+# 项目简述
+
+### 名称
+
+jQuery-Teeterboard
+
+### 简介
+
+基于jQuery的扩展，为网页元素添加鼠标移动时，元素被按压下去的跷跷板效果。
+
+由作者[个人站](https://github.com/xujiaqi0830/Cat-Hub)项目衍生。
+
+### 作者
+
+> PeowXu
+> ##### 个人博客: [http://xujiaqi0830.com/blog](http://xujiaqi0830.com/blog)
+> ##### 邮箱: [xujiaqi0830@163.com](mailto:xujiaqi0830@163.com)
+
+# 主要工具
+
+ - jQuery
+ - CSS3
+
+# 使用方法 
+
+```javascript
+$(selector).addTeeterboard(deg, persp)
+```
+- 参数说明
+ - deg: Int | 倾斜角度，默认3(deg)
+ - persp: Int | 景深，默认800(px)
+
+# 更新日志
+
+### v0.5.0 (Wed, 12 Oct 2016 10:51:23 GMT)
+- 项目上线托管
+- 增加了自定义倾斜角度、景深功能
+
+# 项目结构
+
+- jQuery.Teeterboard.js - 源代码
+- jQuery.Teeterboard.min.js - 压缩后代码
+
+
+# 许可协议
+
+MIT License
+
+Copyright (c) 2016 Jiaqi Xu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ jQuery.Teeterboard.js
@@ -0,0 +1,42 @@
+(function($){
+    $.fn.extend({
+        'addTeeterboard': function(deg, persp){
+            var moveDegree = deg || 3;
+            var perspective = persp || 800;
+            $(this).on('mouseover mousemove', function(ev){
+                var event = ev || window.event;
+                var diffX = ev.pageX - $(this).offset().left - $(this).outerWidth() / 2;
+                var diffY = ev.pageY - $(this).offset().top - $(this).outerHeight() / 2;
+                var tempX = - diffX / ($(this).outerWidth() / 2);
+                var tempY = - diffY / ($(this).outerHeight() / 2);
+                if(diffX < 0 && diffY < 0){
+                    $(this).css({
+                        'transform': 'perspective(' + perspective + 'px) rotateY(' + - moveDegree * ( - Math.pow(tempX, 2) + 2 * tempX) + 'deg) rotateX(' + moveDegree * ( - Math.pow(tempY, 2) + 2 * tempY) + 'deg)'
+                    });
+                }else if(diffX > 0 && diffY < 0){
+                    tempX = - tempX;
+                    $(this).css({
+                        'transform': 'perspective(' + perspective + 'px) rotateY(' + moveDegree * ( - Math.pow(tempX, 2) + 2 * tempX) + 'deg)rotateX(' + moveDegree * ( - Math.pow(tempY, 2) + 2 * tempY) + 'deg)'
+                    });
+                }else if(diffX < 0 && diffY > 0){
+                    tempY = - tempY;
+                    $(this).css({
+                        'transform': 'perspective(' + perspective + 'px) rotateY(' + - moveDegree * ( - Math.pow(tempX, 2) + 2 * tempX) + 'deg) rotateX(' +  - moveDegree * ( - Math.pow(tempY, 2) + 2 * tempY) + 'deg)'
+                    });
+                }else if(diffX > 0 && diffY > 0){
+                    tempX = - tempX;
+                    tempY = - tempY;
+                    $(this).css({
+                        'transform': 'perspective(' + perspective + 'px) rotateY(' + moveDegree * ( - Math.pow(tempX, 2) + 2 * tempX) + 'deg)rotateX(' +  - moveDegree * ( - Math.pow(tempY, 2) + 2 * tempY) + 'deg)'
+                    });
+                }
+            });
+            $(this).on('mouseout', function(){
+                $(this).css({
+                    'transform': 'translateX(0)'
+                });
+            });
+            return $(this);
+        }
+    });
+})(jQuery);
